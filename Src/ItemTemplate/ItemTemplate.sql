1b31e0e54f8f30bc6311978433a30b50df6fc13c
Greg Trevellick <gtrevellick@googlemail.com>
2020-04-28 20:26:27 +0100

    .

--- Src/ItemTemplate/ItemTemplate.sql
+++ Src/ItemTemplate/ItemTemplate.sql
@@ -5,11 +5,9 @@
             WHERE sch.[name] = 'dbo'
 	        AND pro.[name] = 'MySproc')
 BEGIN
-	--Create dummy object
     EXECUTE('CREATE PROCEDURE dbo.MySproc AS SELECT NULL')
 END
 GO
 
---Alter the object rather than delete and recreate so as to retain permissions, plans, history, etc, gregt
 ALTER PROCEDURE dbo.MySproc AS
     SELECT 1
